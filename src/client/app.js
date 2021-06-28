91d5707f3deb52528c99e020b7ba741c6d226dc2
Kacper <kacper.barabasz@wp.pl>
2021-06-28 12:44:12 +0200

    Add birth date page

--- src/client/app.js
+++ src/client/app.js
@@ -1,8 +1,27 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// class App {
-//     constructor() {
+const emailInput = document.querySelector('.main-container__email');
+const placeholder = document.querySelector('.main-container__placeholder');
 
-//     }
-// }
+class App {
+    constructor() {
+        this._handleInputFocusIn();
+        this._handleInputFocusOut();
+    }
+
+    _handleInputFocusIn() {
+        emailInput.addEventListener('focus', e => {
+            placeholder.classList.add('main-container__placeholder--focus');
+        })
+    }
+
+    _handleInputFocusOut() {
+        emailInput.addEventListener('focusout', e => {
+            if (emailInput.value) return;
+            placeholder.classList.remove('main-container__placeholder--focus');
+        })
+    }
+}
+
+const app = new App();
