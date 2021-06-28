1afd1c6319ef4eca4b10a2b6ff2ce254dfee53ea
Kacper <barabasz64@gmail.com>
2021-06-28 21:14:42 +0200

    Add swup library

--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import Swup from 'swup';
 
 const emailForm = document.querySelector('.main-container__form');
 const emailInput = document.querySelector('.main-container__email');
@@ -7,15 +8,18 @@ const placeholder = document.querySelector('.main-container__placeholder');
 const errorIcon = document.querySelector('.main-container__error-icon');
 const errorMsg = document.querySelector('.main-container__error-msg');
 
+// adding swup library for smooth page transition
+const swup = new Swup();
+
 class App {
     constructor() {
 
         // Add event handlers
         emailInput.addEventListener('focus', this._minimizeEmailPlaceholder.bind(this));
         emailInput.addEventListener('focusout', this._normalizeEmailPlaceholder.bind(this));
-        emailInput.addEventListener('focusout', this._checkEmail.bind(this));
+        emailInput.addEventListener('focusout', this._emailValidation.bind(this));
 
-        emailForm.addEventListener('submit', this._checkEmail.bind(this));
+        emailForm.addEventListener('submit', this._emailValidation.bind(this));
     }
 
     _minimizeEmailPlaceholder() {
@@ -27,8 +31,8 @@ class App {
             placeholder.classList.remove('main-container__placeholder--focus');
     }
 
-    _checkEmail(e) {
-        const regex = /@/g;
+    _emailValidation(e) {
+        const regex = /@/;
 
         if (!emailInput.value || !regex.test(emailInput.value)) {
             e.preventDefault();
