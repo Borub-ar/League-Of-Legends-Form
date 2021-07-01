29f5aa323ff40181ea15506ec6434b46b4727e2a
Kacper <kacper.barabasz@wp.pl>
2021-07-01 19:54:01 +0200

    Add last page

--- src/client/birthDate.js
+++ src/client/birthDate.js
@@ -1,14 +1,21 @@
 const birthDateForm = document.querySelector('.main-container__form');
 const formSelects = document.querySelectorAll('.main-container__birth-select');
+const errorMsg = document.querySelector('.main-container__date-error');
 
 const showError = function () {
-    console.log('dasdas')
-}
+    errorMsg.classList.remove('main-container__date-error--hidden');
+};
+
+const hideError = function() {
+    errorMsg.classList.add('main-container__date-error--hidden');
+};
 
 const birthFormValidation = function (e) {
     if ([...formSelects].some(sel => sel.value === '0')) {
         e.preventDefault();
         showError();
+    } else {
+        hideError();
     }
 };
 
