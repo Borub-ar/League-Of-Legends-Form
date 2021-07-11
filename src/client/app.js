e83765f9cbab9f35f0d30294b119eefc77f670d9
Kacper <kacper.barabasz@wp.pl>
2021-07-12 01:15:02 +0200

    Refactor code to make page transitions work

--- src/client/app.js
+++ src/client/app.js
@@ -5,63 +5,96 @@ import 'regenerator-runtime/runtime';
 import Swup from 'swup';
 import SwupFormsPlugin from '@swup/forms-plugin';
 
+// import IndexJS from './app';
+// import BirthDateJS from './birthDate';
 
-const emailForm = document.querySelector('.main-container__form');
-const emailInput = document.querySelector('.main-container__input');
-const placeholder = document.querySelector('.main-container__placeholder');
-const errorIcon = document.querySelector('.main-container__error-icon');
-const errorMsg = document.querySelector('.main-container__error-msg');
+const swup = new Swup({
+    plugins: [new SwupFormsPlugin()]
+});
 
-
-// const swup = new Swup({
-//     plugins: [new SwupFormsPlugin()]
-// });
 
 class App {
     constructor() {
-        // Add event handlers
-        emailInput.addEventListener('focus', this._minimizeEmailPlaceholder.bind(this));
-        emailInput.addEventListener('focusout', this._normalizeEmailPlaceholder.bind(this));
-        emailInput.addEventListener('focusout', this._emailValidation.bind(this));
-        emailForm.addEventListener('submit', this._emailValidation.bind(this));
+        this._init();
+        swup.on('contentReplaced', this._init.bind(this));
     }
 
-    _minimizeEmailPlaceholder() {
-        placeholder.classList.add('main-container__placeholder--focus');
-    }
+    _init() {
+        if (document.querySelector('#index-page')) {
+            const emailForm = document.querySelector('.main-container__form');
+            const emailInput = document.querySelector('.main-container__input');
+            const placeholder = document.querySelector('.main-container__placeholder');
+            const errorIcon = document.querySelector('.main-container__error-icon');
+            const errorMsg = document.querySelector('.main-container__error-msg');
 
-    _normalizeEmailPlaceholder() {
-        if (emailInput.value) return;
-        placeholder.classList.remove('main-container__placeholder--focus');
-    }
+            const minimizeEmailPlaceholder = function() {
+                placeholder.classList.add('main-container__placeholder--focus');
+            }
+        
+            const normalizeEmailPlaceholder = function() {
+                if (emailInput.value) return;
+                placeholder.classList.remove('main-container__placeholder--focus');
+            }
+        
+            const emailValidation = function(e) {
+                const regex = /@/;
+        
+                if (!emailInput.value || !regex.test(emailInput.value)) {
+                    e.preventDefault();
+                    // removing required swup attribute
+                    emailForm.removeAttribute('data-swup-form');
+                    showEmailError();
+                }
+        
+                if (emailInput.value && regex.test(emailInput.value)) {
+                    // setting required swup attribute
+                    emailForm.setAttribute('data-swup-form', '');
+                    hideEmailError();
+                }
+            }
+        
+            const showEmailError = function() {
+                emailInput.classList.add('main-container__input--error');
+                errorIcon.classList.add('main-container__error-icon--show');
+                errorMsg.classList.add('main-container__error-msg--show');
+            }
+        
+            const hideEmailError = function() {
+                emailInput.classList.remove('main-container__input--error');
+                errorIcon.classList.remove('main-container__error-icon--show');
+                errorMsg.classList.remove('main-container__error-msg--show');
+            }
 
-    _emailValidation(e) {
-        const regex = /@/;
-
-        if (!emailInput.value || !regex.test(emailInput.value)) {
-            e.preventDefault();
-            // removing required swup attribute
-            // emailForm.removeAttribute('data-swup-form');
-            this._showEmailError();
+            emailInput.addEventListener('focus', minimizeEmailPlaceholder);
+            emailInput.addEventListener('focusout', normalizeEmailPlaceholder);
+            emailInput.addEventListener('focusout', emailValidation);
+            emailForm.addEventListener('submit', emailValidation);
         }
 
-        if (emailInput.value && regex.test(emailInput.value)) {
-            // setting required swup attribute
-            // emailForm.setAttribute('data-swup-form', '');
-            this._hideEmailError();
+        if (document.querySelector('#birth-date-page')) {
+            const birthDateForm = document.querySelector('.main-container__form');
+            const formSelects = document.querySelectorAll('.custom-select__select');
+            const errorMsg = document.querySelector('.main-container__date-error');
+
+            const birthFormValidation = function (e) {
+                if ([...formSelects].some(sel => sel.value === '0')) {
+                    e.preventDefault();
+                    showError();
+                } else hideError();
+            }
+
+            const showError = function () {
+                errorMsg.classList.remove('main-container__date-error--hidden');
+            }
+
+            const hideError = function () {
+                errorMsg.classList.add('main-container__date-error--hidden');
+            }
+
+
+            birthDateForm.addEventListener('submit', birthFormValidation);
+            formSelects.forEach(sel => sel.addEventListener('focusout', birthFormValidation));
         }
-    }
-
-    _showEmailError() {
-        emailInput.classList.add('main-container__input--error');
-        errorIcon.classList.add('main-container__error-icon--show');
-        errorMsg.classList.add('main-container__error-msg--show');
-    }
-
-    _hideEmailError() {
-        emailInput.classList.remove('main-container__input--error');
-        errorIcon.classList.remove('main-container__error-icon--show');
-        errorMsg.classList.remove('main-container__error-msg--show');
     }
 }
 
--- src/client/birthDate.js
+++ src/client/birthDate.js
@@ -1,22 +1,47 @@
+// import {construct
+// } from "core-js/fn/reflect";
+
+
+
+// const showError = function () {
+//     errorMsg.classList.remove('main-container__date-error--hidden');
+// };
+
+// const hideError = function () {
+//     errorMsg.classList.add('main-container__date-error--hidden');
+// };
+
+// const birthFormValidation = function (e) {
+//     if ([...formSelects].some(sel => sel.value === '0')) {
+//         e.preventDefault();
+//         showError();
+//     } else hideError();
+// };
+
+// formSelects.forEach(sel => sel.addEventListener('focusout', birthFormValidation));
+
 const birthDateForm = document.querySelector('.main-container__form');
-const formSelects = document.querySelectorAll('.main-container__birth-select');
+const formSelects = document.querySelectorAll('.custom-select__select');
 const errorMsg = document.querySelector('.main-container__date-error');
 
-const showError = function () {
-    errorMsg.classList.remove('main-container__date-error--hidden');
-};
+export default class BirthDate {
+    constructor() {
+        birthDateForm.addEventListener('submit', this._birthFormValidation.bind(this));
+        formSelects.forEach(sel => sel.addEventListener('focusout', this._birthFormValidation.bind(this)));
+    }
 
-const hideError = function() {
-    errorMsg.classList.add('main-container__date-error--hidden');
-};
+    _birthFormValidation(e) {
+        if ([...formSelects].some(sel => sel.value === '0')) {
+            e.preventDefault();
+            this._showError();
+        } else this._hideError();
+    }
 
-const birthFormValidation = function (e) {
-    if ([...formSelects].some(sel => sel.value === '0')) {
-        e.preventDefault();
-        showError();
-    } else hideError();
-    
-};
+    _showError() {
+        errorMsg.classList.remove('main-container__date-error--hidden');
+    }
 
-birthDateForm.addEventListener('submit', birthFormValidation);
-formSelects.forEach(sel => sel.addEventListener('focusout', birthFormValidation));
+    _hideError() {
+        errorMsg.classList.add('main-container__date-error--hidden');
+    }
+}
