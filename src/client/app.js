38251942c7aa730cb8dedfa405412b8c180b347b
Kacper <kacper.barabasz@wp.pl>
2021-07-13 19:11:46 +0200

    Add dynamic module imports

--- src/client/app.js
+++ src/client/app.js
@@ -5,10 +5,6 @@ import 'regenerator-runtime/runtime';
 import Swup from 'swup';
 import SwupFormsPlugin from '@swup/forms-plugin';
 
-import IndexJS from './index';
-import BirthDateJS from './birthDate';
-import LastStepJS from './lastStep';
-
 const swup = new Swup({
     plugins: [new SwupFormsPlugin()]
 });
@@ -21,19 +17,23 @@ class App {
 
     _init() {
         if (document.querySelector('#index-page')) {
-            const index = new IndexJS();
+            import('./index').then(({ default: IndexJS }) => {
+                const index = new  IndexJS;
+            })
         }
 
         if (document.querySelector('#birth-date-page')) {
-            const birthDate = new BirthDateJS();
+            import('./birthDate').then(({ default: BirthDateJS }) => {
+                const birthDate = new BirthDateJS();
+            })
         }
 
         if (document.querySelector('#last-step-page')) {
-            const lastStep = new LastStepJS();
+            import('./lastStep').then(({ default: LastStepJS }) => {
+                const lastStep = new LastStepJS();
+            })
         }
     }
 }
 
 const app = new App();
-
-console.log()
