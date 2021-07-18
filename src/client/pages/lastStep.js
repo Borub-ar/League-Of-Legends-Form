29c5015e6b5897f7da3e62a917a307d6e2efee6a
Kacper <barabasz64@gmail.com>
2021-07-18 17:16:22 +0200

    Fix error messages rendering

--- src/client/pages/lastStep.js
+++ src/client/pages/lastStep.js
@@ -5,26 +5,47 @@ export default class App extends Common {
     placeholders = document.querySelectorAll('.main-container__placeholder');
     checkboxes = document.querySelectorAll('.main-container__checkbox');
     checkboxLabels = document.querySelectorAll('.main-container__checkbox-label');
+    requiredCheckbox = document.querySelector('[data-required]');
+    usernameInput = document.querySelector('[data-username]');
+
+    
+    checkboxError = document.querySelector('[data-checkboxError]');
+    usernameFirstError = document.querySelector('[data-usernameFirstError]');
+
 
     constructor() {
         super();
         this._handleEvents();
     }
 
-    _pageValidation(e) {
-        if (![...this.checkboxes].every(el => el.classList.contains('main-container__checkbox--marked'))) {
-            e.preventDefault();
-        }
+    _usernameValidation() {
+        const firstRegex = /^[a-zA-Z, 0-9]{3,}$/;
+ 
+        if (!firstRegex.test(this.usernameInput.value)) {
+            this.usernameFirstError.style.display = 'block';
+        } else this.usernameFirstError.style.display = 'none';
+        
+
+        const secondRegex = /b/;
+        const thirdRegex = /c/;
     }
 
+    // _checkboxValidation(e) {
+    //     if (this.requiredCheckbox.classList.contains('main-container__checkbox--marked')) {
+    //         e.preventDefault();
+    //         this.checkboxError.style.display = 'block';
+    //         return
+    //     }
+    // }
+
     _placeholderFocus(e) {
-    e.target.parentElement.querySelector('.main-container__placeholder').classList.add('main-container__placeholder--small-focus');
+        e.target.parentElement.querySelector('.main-container__placeholder').classList.add('main-container__placeholder--small-focus');
     }
 
     _placeholderNormalize() {
         [...this.inputs].forEach(input => {
-            if(input.value) return
-            if(!input.value) {
+            if (input.value) return
+            if (!input.value) {
                 input.parentElement.querySelector('.main-container__placeholder').classList.remove('main-container__placeholder--small-focus');
             }
         })
@@ -39,6 +60,6 @@ export default class App extends Common {
         [...this.inputs].forEach(input => input.addEventListener('focusout', this._placeholderNormalize.bind(this)));
         [...this.checkboxes].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
         [...this.checkboxLabels].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
-        this.form.addEventListener('submit', this._pageValidation.bind(this));
+        this.usernameInput.addEventListener('focusout', this._usernameValidation.bind(this));
     }
 }
