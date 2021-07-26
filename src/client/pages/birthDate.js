daed90fb7a66c19f64f23174ff86b543049bc04e
Kacper <barabasz64@gmail.com>
2021-07-26 19:58:33 +0200

    Refactor code

--- src/client/pages/birthDate.js
+++ src/client/pages/birthDate.js
@@ -2,40 +2,18 @@ import Common from '../common';
 
 export default class App extends Common {
     _formSelects = document.querySelectorAll('.custom-select__select--birth-date');
-    _errorMsg = document.querySelector('.main-container__error-msg');
+    _errorMsg = document.querySelector('[data-birthError]');
+    _selectDay = document.querySelector('[data-day]');
+    _selectYear = document.querySelector('[data-year]');
 
     constructor() {
         super();
+        this._generateSelectOptions();
         this._handleEvents();
     }
 
-    _birthFormValidation(e) {
-        if ([...this._formSelects].some(sel => sel.value === '0')) {
-            e.preventDefault();
-            this._removeSwupAttribute();
-            this._showError();
-        } else {
-            this._setSwupAttribute();
-            this._hideError();
-        }
-    }
-
-    _checkIfCorrect() {
-        if (![...this._formSelects].some(sel => sel.value === '0')) {
-            this._hideError();
-        }
-    }
-    
-    _showError() {
-        this._errorMsg.classList.add('main-container__error-msg--show');
-    }
-    
-    _hideError() {
-        this._errorMsg.classList.remove('main-container__error-msg--show');
-    }
-
     _handleEvents() {
-        // this._form.addEventListener('submit', this._birthFormValidation.bind(this));
         this._formSelects.forEach(sel => sel.addEventListener('change', this._checkIfCorrect.bind(this)));
+        // this._form.addEventListener('submit', this._birthFormValidation.bind(this));
     }
 }
--- src/client/pages/index.js
+++ src/client/pages/index.js
@@ -9,37 +9,39 @@ export default class App extends Common {
     constructor() {
         super();
         this._handleEvents();
+        localStorage.clear();
     }
 
     _placeholderFocus() {
         this._placeholder.classList.add('main-container__placeholder--focus');
     }
-    
+
     _normalizePlaceholder() {
         if (this._emailInput.value) return;
         this._placeholder.classList.remove('main-container__placeholder--focus');
     }
-    
-    _emailValidation (e) {
+
+    _emailValidation(e) {
         const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    
+
         if (!this._emailInput.value || !regex.test(this._emailInput.value)) {
             e.preventDefault();
             this._removeSwupAttribute();
             this._showEmailError(e);
         }
-    
+
         if (this._emailInput.value && regex.test(this._emailInput.value)) {
-            this._setSwupAttribute();
+            this._setLocalStorage();
             this._hideEmailError();
+            this._setSwupAttribute();
         }
     }
-    
+
     _showEmailError(e) {
-        this._renderErrorElements(e);
+        // this._renderErrorElements(e);
         this._errorMsg.classList.add('main-container__error-msg--show');
     }
-    
+
     _hideEmailError() {
         this._emailInput.classList.remove('main-container__input--error');
         this._errorIcon.classList.remove('main-container__error-icon--show');
@@ -53,12 +55,3 @@ export default class App extends Common {
         // this._form.addEventListener('submit', this._emailValidation.bind(this));
     }
 }
-
-
-
-
-
-
-
-
-
