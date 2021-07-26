47a76bb4911ef20156b7f6c8dee048a15606c179
Kacper <kacper.barabasz@wp.pl>
2021-07-26 12:41:47 +0200

    Finish 'download page' layout

--- src/client/pages/lastStep.js
+++ src/client/pages/lastStep.js
@@ -1,12 +1,28 @@
 import Common from '../common';
 
 export default class App extends Common {
-    inputs = document.querySelectorAll('.main-container__input');
-    placeholders = document.querySelectorAll('.main-container__placeholder');
-    checkboxes = document.querySelectorAll('.main-container__checkbox');
-    checkboxLabels = document.querySelectorAll('.main-container__checkbox-label');
-    requiredCheckbox = document.querySelector('[data-required]');
-    usernameInput = document.querySelector('[data-username]');
+    // General
+    _inputs = document.querySelectorAll('.main-container__input');
+    _placeholders = document.querySelectorAll('.main-container__placeholder');
+    _checkboxes = document.querySelectorAll('.main-container__checkbox');
+    _checkboxLabels = document.querySelectorAll('.main-container__checkbox-label');
+    // UsernameInput
+    _usernameInput = document.querySelector('[data-username]');
+    _usernameFirstError = document.querySelector('[data-usernameFirstError]');
+    _usernameSecondError = document.querySelector('[data-usernameSecondError]');
+    _usernameThirdError = document.querySelector('[data-usernameThirdError]');
+    // Password input
+    _passwordInput = document.querySelector('[data-password]');
+    _passwordFirstError = document.querySelector('[data-passwordFirstError]');
+    _passwordSecondError = document.querySelector('[data-passwordSecondError]');
+    // Repeat password input
+    _repeatPassInput = document.querySelector('[data-repeat]');
+    _repeatFirstError = document.querySelector('[data-repeatFirstError]')
+    _repeatSecondError = document.querySelector('[data-repeatSecondError]')
+    // Checkbox
+    _checkboxCon = document.querySelector('[data-checkboxCon]');
+    _requiredCheckbox = document.querySelector('[data-required]');
+    _checkboxError = document.querySelector('[data-checkboxError]');
 
     constructor() {
         super();
@@ -14,44 +30,135 @@ export default class App extends Common {
     }
 
     _usernameValidation() {
-        const usernameFirstError = document.querySelector('[data-usernameFirstError]');
-        const usernameSecondError = document.querySelector('[data-usernameSecondError]');
-        const usernameThirdError = document.querySelector('[data-usernameThirdError]');
+        const checkMinLength = () => {
+            const firstRegex = /^^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~, a-zA-Z, 0-9]{4,}$/;
+            if (this._inputValidation(firstRegex, this._usernameFirstError, this._usernameInput.value)) return true;
+            else return false;
+        };
 
-        const firstRegex = /^[a-zA-Z, 0-9]{3,}$/;
+        const checkMaxLength = () => {
+            if (this._usernameInput.value.length >= 25) {
+                this._usernameSecondError.style.display = 'block';
+                // this._renderErrorElements(e);
+                return false;
+            } else {
+                this._usernameSecondError.style.display = 'none';
+                return true;
+            }
+        };
 
-        if (!firstRegex.test(this.usernameInput.value)) {
-            usernameFirstError.style.display = 'block';
-        } else usernameFirstError.style.display = 'none';
+        const checkSpaces = () => {
+            if (this._usernameInput.value.includes(' ')) {
+                // this._renderErrorElements(e);
+                this._usernameThirdError.style.display = 'block';
+                return false
+            } else {
+                this._usernameThirdError.style.display = 'none';
+                return true
+            }
+        };
 
-        const secondRegex = /^[a-zA-Z, 0-9]{3,25}$/
-
-        if (!secondRegex.test(this.usernameInput.value)) {
-            usernameSecondError.style.display = 'block';
-        } else usernameSecondError.style.display = 'none';
-
-        if (this.usernameInput.value.includes(' ')) {
-            usernameThirdError.style.display = 'block';
-        } else usernameThirdError.style.display = 'none';
-       
+        if (checkMinLength() && checkMaxLength() && checkSpaces()) {
+            // this._removeError(e);
+            return true;
+        } else return false;
     }
 
-    // _checkboxValidation(e) {
-    // const checkboxError = document.querySelector('[data-checkboxError]');
+    _passwordValidation() {
+        const checkMinLength = () => {
+            const firstRegex = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~, a-zA-Z, 0-9]{8,}$/;
+            if (this._inputValidation(firstRegex, this._passwordFirstError, this._passwordInput.value)) return true;
+            else return false
+        }
 
-    //     if (this.requiredCheckbox.classList.contains('main-container__checkbox--marked')) {
-    //         e.preventDefault();
-    //         this.checkboxError.style.display = 'block';
-    //         return
-    //     }
-    // }
+        const checkLetters = () => {
+            const secondRegex = /[a-zA-Z]/;
+            if (this._inputValidation(secondRegex, this._passwordSecondError, this._passwordInput.value)) return true;
+            else return false
+        }
+
+        const checkSpecialChar = () => {
+            const thirdRegex = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~, 0-9]/;
+            if (this._inputValidation(thirdRegex, this._passwordSecondError, this._passwordInput.value)) return true;
+            else return false;
+        }
+
+        if (checkMinLength() && checkLetters() && checkSpecialChar()) {
+            // this._removeError(e);
+            return true;
+        } else return false;
+    }
+
+    _repeatPassValidation() {
+        const comparePass = () => {
+            if (this._passwordInput.value !== this._repeatPassInput.value) {
+                // this._renderErrorElements(e);
+                this._repeatFirstError.style.display = 'block';
+                return false;
+            } else {
+                this._repeatFirstError.style.display = 'none';
+                return true;
+            }
+        }
+
+        const checkIfEmpty = () => {
+            if (!this._repeatPassInput.value) {
+                // this._renderErrorElements(e);
+                this._repeatSecondError.style.display = 'block';
+                return false;
+            } else {
+                this._repeatSecondError.style.display = 'none';
+                return true;
+            }
+        };
+
+        if (checkIfEmpty() && comparePass()) {
+            // this._removeError(e);
+            return true;
+        } else return false;
+    }
+
+    _inputValidation(regex, errorNumber, inputValue) {
+        if (!regex.test(inputValue)) {
+            // this._renderErrorElements(e);
+            errorNumber.style.display = 'block';
+            return false
+        } else {
+            errorNumber.style.display = 'none';
+            return true
+        }
+    }
+
+    _checkboxValidation(e) {
+        if (!this._requiredCheckbox.classList.contains('main-container__checkbox--marked')) {
+            this._requiredCheckbox.style.borderColor = '#be1e37';
+            this._checkboxError.style.display = 'block';
+            this._checkboxCon.style.marginTop = '2rem';
+            return false
+        } else {
+            this._requiredCheckbox.style.borderColor = '#937341';
+            this._checkboxError.style.display = 'none';
+            this._checkboxCon.style.marginTop = '.7rem';
+            return true
+        }
+    }
+
+    _pageValidation(e) {
+        if (!this._usernameValidation() && !this._passwordValidation() && !this._repeatPassValidation() && !this._checkboxValidation()) {
+            e.preventDefault();
+        } 
+        
+        if (this._usernameValidation() && this._passwordValidation() && this._repeatPassValidation() && this._checkboxValidation()) {
+            return
+        } else e.preventDefault();
+    }
 
     _placeholderFocus(e) {
         e.target.parentElement.querySelector('.main-container__placeholder').classList.add('main-container__placeholder--small-focus');
     }
 
     _placeholderNormalize() {
-        [...this.inputs].forEach(input => {
+        [...this._inputs].forEach(input => {
             if (input.value) return
             if (!input.value) {
                 input.parentElement.querySelector('.main-container__placeholder').classList.remove('main-container__placeholder--small-focus');
@@ -64,10 +171,14 @@ export default class App extends Common {
     }
 
     _handleEvents() {
-        [...this.inputs].forEach(input => input.addEventListener('focus', this._placeholderFocus.bind(this)));
-        [...this.inputs].forEach(input => input.addEventListener('focusout', this._placeholderNormalize.bind(this)));
-        [...this.checkboxes].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
-        [...this.checkboxLabels].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
-        this.usernameInput.addEventListener('focusout', this._usernameValidation.bind(this));
+        [...this._inputs].forEach(input => input.addEventListener('focus', this._placeholderFocus.bind(this)));
+        [...this._inputs].forEach(input => input.addEventListener('focusout', this._placeholderNormalize.bind(this)));
+        [...this._checkboxes].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
+        [...this._checkboxLabels].forEach(checkbox => checkbox.addEventListener('click', this._markCheckbox.bind(this)));
+        this._requiredCheckbox.addEventListener('click', this._checkboxValidation.bind(this));
+        this._usernameInput.addEventListener('focusout', this._usernameValidation.bind(this));
+        this._passwordInput.addEventListener('focusout', this._passwordValidation.bind(this));
+        this._repeatPassInput.addEventListener('focusout', this._repeatPassValidation.bind(this));
+        // this._form.addEventListener('submit', this._pageValidation.bind(this));
     }
 }
