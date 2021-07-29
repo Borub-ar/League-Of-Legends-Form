d2cfc481c1e9cfdcfb0e25ffea2a190d056a285d
Kacper <kacper.barabasz@wp.pl>
2021-07-29 20:50:36 +0200

    Fix minor bugs

--- src/client/pages/summary.js
+++ src/client/pages/summary.js
@@ -5,6 +5,7 @@ export default class App extends Common {
 
     constructor() {
         super();
+        this._handleEvents();
         this._handleValidationEvents();
         this._generateSelectOptions();
         this._setInputValues();
@@ -22,6 +23,19 @@ export default class App extends Common {
         checkboxMarked === 'true' ? this._optionalCheckbox.classList.add('main-container__checkbox--marked') : '';
     }
 
+    _placeholderFocus(e) {
+        e.target.parentElement.querySelector('.main-container__placeholder').classList.add('main-container__placeholder--focus-summary');
+    }
+
+    _placeholderNormalize() {
+        [...this._inputs].forEach(input => {
+            if (input.value) return
+            if (!input.value) {
+                input.parentElement.querySelector('.main-container__placeholder').classList.remove('main-container__placeholder--focus-summary');
+            }
+        })
+    }
+
     _pageValidation(e) {
         if (!this._usernameValidation() || !this._passwordValidation() || !this._repeatPassValidation() || !this._checkboxValidation() || this._emailValidation()) {
             e.preventDefault();
@@ -32,5 +46,10 @@ export default class App extends Common {
         if (this._usernameValidation() && this._passwordValidation() && this._repeatPassValidation() && this._checkboxValidation() && this._emailValidation()) {
             this._setSwupAttribute();
         } else e.preventDefault();
+    }
+    
+    _handleEvents() {
+        [...this._inputs].forEach(input => input.addEventListener('focus', this._placeholderFocus.bind(this)));
+        [...this._inputs].forEach(input => input.addEventListener('focusout', this._placeholderNormalize.bind(this)));
     }
 }
