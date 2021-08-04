2ac6bb6542a836555ae8a6ac2d9fb31f9de47562
Kacper <barabasz64@gmail.com>
2021-08-04 11:15:44 +0200

    fix minor layout problems

--- src/client/common.js
+++ src/client/common.js
@@ -122,14 +122,14 @@ export default class Common {
     }
 
     _emailValidation() {
-        const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        const emailVal = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-        if (!this._emailInput.value || !regex.test(this._emailInput.value)) {
+        if (!this._emailInput.value || !emailVal.test(this._emailInput.value)) {
             this._renderInputErr('email', 'emailFirstError', 'emailCon');
             return false;
         }
 
-        if (this._emailInput.value && regex.test(this._emailInput.value)) {
+        if (this._emailInput.value && emailVal.test(this._emailInput.value)) {
             this._setLocalStorage();
             this._hideInputErr('email', 'emailFirstError', 'emailCon');
             return true;
@@ -184,9 +184,9 @@ export default class Common {
         }
 
         const checkLetters = () => {
-            const secondRegex = /[a-zA-Z]/;
+            const hasLetters = /[a-zA-Z]/;
 
-            if (!secondRegex.test(this._passwordInput.value)) {
+            if (!hasLetters.test(this._passwordInput.value)) {
                 this._renderInputErr('password', 'passwordSecondError', 'passwordCon');
                 return false;
             } else {
@@ -196,9 +196,9 @@ export default class Common {
         }
 
         const checkSpecialChar = () => {
-            const thirdRegex = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~, 0-9]/g;
+            const hasSpecialChar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~, 0-9]/g;
 
-            if (!thirdRegex.test(this._passwordInput.value)) {
+            if (!hasSpecialChar.test(this._passwordInput.value)) {
                 this._renderInputErr('password', 'passwordSecondError', 'passwordCon');
                 return false;
             } else {
